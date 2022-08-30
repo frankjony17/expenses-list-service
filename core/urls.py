304b807e6d525dce941e9b3ef38d1fb5b5ceda5a
Frank Ricardo Ramirez <frankjony17@gmail.com>
2022-08-30 15:52:57 -0300

    feature: add swagger and api-token auth

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,28 @@
-"""core URL Configuration
+from django.urls import path, re_path, include
+from rest_framework.authtoken import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Expenses Shopping List",
+        default_version='v1',
+        description="The Smart Expenses and Shopping List App",
+        # terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="frankjony17@gmail.com"),
+        license=openapi.License(name="GNU GENERAL PUBLIC LICENSE"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   # Route TemplateView to serve Swagger UI template.
+   re_path(r'^swagger-ui(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   re_path(r'^swagger-ui/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+   path('api-token-auth/', views.obtain_auth_token, name='obtain_auth_token'),
+   path('', include('rest_framework.urls', namespace='rest_framework')),
 ]
