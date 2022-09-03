f939f74855d2356f870520695562dfd139a8b8b9
Frank Ricardo Ramirez <frankjony17@gmail.com>
2022-09-03 14:27:48 -0300

    feature: user is not staff they can create Products for their own use.

---
+++ products/receivers.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from products.models import Products
+
+
+@receiver(pre_save, sender=Products)
+def pre_save_products_receiver(sender, instance, raw, using, **kwargs):
+    if instance.user.is_staff or instance.user.is_superuser:
+        instance.user = None
+
+
+pre_save.connect(pre_save_products_receiver, sender=Products)
